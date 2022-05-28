52e0006a0deb6c060879048af58d0d07559247b4
Higeuni <khyogeun_@naver.com>
2022-05-28 13:09:22 +0900

    [#9] Modulization And make crawler

---
+++ summerizer.py
@@ -0,0 +1,36 @@
+from ipaddress import summarize_address_range
+import sys
+from crawler import getNewsContent
+from textRank import *
+
+def summarize_contents(x):
+    tr = TextRank()
+
+    stopword = set([('있', 'VV'), ('하', 'VV'), ('되', 'VV') ])
+    tr.loadSents(RawSentence(x), lambda sent: filter(lambda y:y not in stopword and y[1] in ('NNG', 'NNP', 'VV', 'VA'), tagger.pos(sent)))
+    
+    tr.build()
+    ranks = tr.rank()
+    return tr.summarize(0.2)
+
+if __name__ == "__main__":
+    print("Select Mode - 1 : News, 2 : Custom Text")
+    mode = int(input())
+    if mode == 1:
+        print("Input Url : ", end='')
+        url = input()
+        try:
+            content = getNewsContent(url)
+            print(summarize_contents(content))
+        except:
+            pass
+    else:
+        print("Input Text all and input 'end' : ", end='')
+        x = ''
+        while True:
+            s = input()
+            if s == "end":
+                break
+            x += s
+            
+        print(summarize_contents(x))
